2170ec782908de9caf40282626ffae25b089aa86
theMugician <gslonina7@gmail.com>
2021-10-17 14:25:34 -0700

    add comments using jsdoc

--- splash-audio-player.js
+++ splash-audio-player.js
@@ -1,55 +1,70 @@
 class SplashAudioPlayer {
+    /**
+     * Create an instance of SplashAudioPlayer
+     * @param {string} player 
+     */
     constructor(player) {
         this.audioPlayer = typeof player === 'string' ? document.querySelector(player) : player
 
-        this.audioElementHTML = this.audioPlayer.innerHTML
-        this.audioElement = this.audioPlayer.getElementsByTagName('audio')[0]
-        this.audioPlayer.classList.add('splash-audio-player')
-        this.audioPlayer.innerHTML = SplashAudioPlayer.getTemplate() + this.audioElementHTML
+        /**
+         * Get inner HTML of audio player
+         */      
+        this.audioTrackHTML = this.audioPlayer.innerHTML
+        
+        /**
+         * Access DOM element <audio> tag
+         */        
+        this.audioTrack = this.audioPlayer.getElementsByTagName('audio')[0]
 
+        /**
+         * Add class 'splash-audio-player' to audio player
+         * @see audioPlayer
+         */   
+        this.audioPlayer.classList.add('splash-audio-player')
+
+        /**
+         * Create HTML template of audio player
+         * @see getTemplate
+         */
+        this.audioPlayer.innerHTML = SplashAudioPlayer.getTemplate() + this.audioTrackHTML
+
+        /**
+         * Get DOM elements from audio player
+         * @see audioPlayer
+         */
         this.seekSlider = this.audioPlayer.getElementsByClassName('splash-audio-player__seek-slider')[0]
         this.playPauseButton = this.audioPlayer.getElementsByClassName('splash-audio-player__play-pause-button')[0]
         this.playPauseButtonIcon = this.audioPlayer.getElementsByClassName('play-pause-button__icon')[0]
         this.volumeSlider = this.audioPlayer.getElementsByClassName('splash-audio-player__volume-slider')[0]
         this.muteButton = this.audioPlayer.getElementsByClassName('splash-audio-player__mute-button')[0]
         this.muteButtonIcon = this.audioPlayer.getElementsByClassName('mute-button__icon')[0]
-
         this.durationContainer = this.audioPlayer.getElementsByClassName('splash-audio-player__duration')[0]
         this.currentTimeContainer = this.audioPlayer.getElementsByClassName('splash-audio-player__current-time')[0]
         this.outputContainer = this.audioPlayer.getElementsByClassName('splash-audio-player__volume-output')[0]
-        
-        this.playState = 'play'
+
+        /**
+         * Local State
+         */       
+        this.playState = 'pause'
         this.muteState = 'unmute'
         this.raf = null
 
+        /**
+         * Initiate an instance of the audio player plugin
+         * @see init
+         */
         this.init()
     }
+    
 
     emitEvents() {
+
         this.playPauseButton.addEventListener('click', () => {
-            if(this.playState === 'play') {
-                this.audioElement.play()
-                requestAnimationFrame(this.whilePlaying)
-                this.playState = 'pause'
-                this.playPauseButtonIcon.attributes.d.value = 'M0 0h6v24H0zM12 0h6v24h-6z'
-            } else {
-                this.audioElement.pause()
-                cancelAnimationFrame(this.raf)
-                this.playState = 'play'
-                this.playPauseButtonIcon.attributes.d.value = 'M18 12L0 24V0'
-            }
+            this.playHandler()
         })
 
         this.muteButton.addEventListener('click', () => {
-            if(this.muteState === 'unmute') {
-                this.audioElement.muted = true
-                this.muteState = 'mute'
-                this.muteButtonIcon.attributes.d.value = 'M0 7.667v8h5.333L12 22.333V1L5.333 7.667'
-            } else {
-                this.audioElement.muted = false
-                this.muteState = 'unmute'
-                this.muteButtonIcon.attributes.d.value = 'M14.667 0v2.747c3.853 1.146 6.666 4.72 6.666 8.946 0 4.227-2.813 7.787-6.666 8.934v2.76C20 22.173 24 17.4 24 11.693 24 5.987 20 1.213 14.667 0zM18 11.693c0-2.36-1.333-4.386-3.333-5.373v10.707c2-.947 3.333-2.987 3.333-5.334zm-18-4v8h5.333L12 22.36V1.027L5.333 7.693H0z'
-            }
+            this.muteHandler()
         })
 
         this.seekSlider.addEventListener('input', (e) => {
@@ -61,21 +76,21 @@ class SplashAudioPlayer {
 
         this.seekSlider.addEventListener('input', () => {
             this.currentTimeContainer.textContent = this.calculateTime(this.seekSlider.value)
-            if(!this.audioElement.paused) {
+            if(!this.audioTrack.paused) {
                 cancelAnimationFrame(this.raf)
             }
         })
     
         this.seekSlider.addEventListener('change', () => {
-            this.audioElement.currentTime = this.seekSlider.value
-            if(!this.audioElement.paused) {
+            this.audioTrack.currentTime = this.seekSlider.value
+            if(!this.audioTrack.paused) {
                 requestAnimationFrame(this.whilePlaying)
             }
         })
     
         this.volumeSlider.addEventListener('input', (e) => {
             const value = e.target.value
-            this.audioElement.volume = value / 100
+            this.audioTrack.volume = value / 100
         })
     }
     
@@ -97,11 +112,43 @@ class SplashAudioPlayer {
 
     /** Implementation of the functionality of the audio player */
 
+    playHandler = () => {
+        this.playState = this.playState === 'pause' ? 'play' : 'pause'
+        if(this.playState === 'play') {
+            this.audioTrack.play()
+            requestAnimationFrame(this.whilePlaying)
+            this.playPauseButtonIcon.attributes.d.value = 'M0 0h6v24H0zM12 0h6v24h-6z'
+        } else {
+            this.audioTrack.pause()
+            cancelAnimationFrame(this.raf)
+            this.playPauseButtonIcon.attributes.d.value = 'M18 12L0 24V0'
+        }
+    }
+
+    muteHandler = () => {
+        this.muteState = this.muteState === 'unmute' ? 'mute' : 'unmute'
+        if(this.muteState === 'mute') {
+            this.audioTrack.muted = true
+            this.muteButtonIcon.attributes.d.value = 'M0 7.667v8h5.333L12 22.333V1L5.333 7.667'
+        } else {
+            this.audioTrack.muted = false
+            this.muteButtonIcon.attributes.d.value = 'M14.667 0v2.747c3.853 1.146 6.666 4.72 6.666 8.946 0 4.227-2.813 7.787-6.666 8.934v2.76C20 22.173 24 17.4 24 11.693 24 5.987 20 1.213 14.667 0zM18 11.693c0-2.36-1.333-4.386-3.333-5.373v10.707c2-.947 3.333-2.987 3.333-5.334zm-18-4v8h5.333L12 22.36V1.027L5.333 7.693H0z'
+        }
+    }    
+
+    /**
+     * @param {number} rangeInput
+     * @return {null}
+     */
     showRangeProgress = (rangeInput) => {
         if(rangeInput === this.seekSlider) this.audioPlayer.style.setProperty('--seek-slider-before__width', rangeInput.value / rangeInput.max * 100 + '%')
         else this.audioPlayer.style.setProperty('--volume-slider-before__width', rangeInput.value / rangeInput.max * 100 + '%')
     }
 
+    /**
+     * @param {number} secs
+     * @return {string} 00:00
+     */
     calculateTime = (secs) => {
         const minutes = Math.floor(secs / 60)
         const seconds = Math.floor(secs % 60)
@@ -109,16 +156,25 @@ class SplashAudioPlayer {
         return `${minutes}:${returnedSeconds}`
     }
 
+    /**
+     * Display the length/duration of the audio track
+     */
     displayDuration = () => {
-        this.durationContainer.textContent = this.calculateTime(this.audioElement.duration)
+        this.durationContainer.textContent = this.calculateTime(this.audioTrack.duration)
     }
 
+    /**
+     * Set the max length of the seek slider based on the duration of the audio track
+     */
     setSliderMax = () => {
-        this.seekSlider.max = Math.floor(this.audioElement.duration)
+        this.seekSlider.max = Math.floor(this.audioTrack.duration)
     }
 
+    /**
+     * Update the time of the track and the position of the seek slider while playing
+     */
     whilePlaying = () => {
-        this.seekSlider.value = Math.floor(this.audioElement.currentTime)
+        this.seekSlider.value = Math.floor(this.audioTrack.currentTime)
         this.currentTimeContainer.textContent = this.calculateTime(this.seekSlider.value)
         this.audioPlayer.style.setProperty('--seek-slider-before__width', `${this.seekSlider.value / this.seekSlider.max * 100}%`)
         this.raf = requestAnimationFrame(this.whilePlaying)
@@ -126,11 +182,11 @@ class SplashAudioPlayer {
 
     init() {
         this.emitEvents()
-        if (this.audioElement.readyState > 0) {
+        if (this.audioTrack.readyState > 0) {
             this.displayDuration()
             this.setSliderMax()
         } else {
-            this.audioElement.addEventListener('loadedmetadata', () => {
+            this.audioTrack.addEventListener('loadedmetadata', () => {
                 this.displayDuration()
                 this.setSliderMax()
             })
